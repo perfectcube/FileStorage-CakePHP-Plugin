453222102eb1dc35ed16c046a5dac3ce21678ca0
Buildrr.com <support@buildrr.com>
2014-09-17 15:24:39 +0000

    quick return empty mimetypes

--- Controller/FileStorageAppController.php
+++ Controller/FileStorageAppController.php
@@ -8,15 +8,18 @@ App::uses('FileStorageAppController', 'Controller');
  * @license MIT
  */
 class FileStorageAppController extends AppController {
-	
-	
+
+
 	/**
 	 * Simple Method for detecting what model to save to
 	 * by file mime_type
 	 */
-	
+
 	protected function _detectModelByFileType ($mime_type) {
+		if (empty($mime_type)) {
+			return false;
+		}
 		return FileStorageUtils::detectModelByFileType($mime_type);
 	}
-	
+
 }
